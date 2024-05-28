01d8ab5b642815f7f2721881e4db73eefaadd045
zhuyingying <yingying.zhu@cn.aesc-group.com>
2024-05-28 09:26:17 +0800

    add program compare

---
+++ CompareProgram/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+var builder = new HostBuilder()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory());
+        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+    });
+
+await builder.Build().RunAsync();
+
+var host = builder.Build();
+using (host)
+{
+    var config = host.Services.GetRequiredService<IConfiguration>();
+    // 使用config
+    Console.WriteLine(config["ConnectionStrings"]);
+}
