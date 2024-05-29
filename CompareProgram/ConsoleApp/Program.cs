bf5c4dddbc93980aaa96eb260909022fc6495c85
zhuyingying <yingying.zhu@cn.aesc-group.com>
2024-05-29 16:59:46 +0800

    add host

--- CompareProgram/ConsoleApp/Program.cs
+++ CompareProgram/ConsoleApp/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace ConsoleApp
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //static async Task Main(string[] args)
+        //{            
+        //    HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+        //    builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+        //    builder.Configuration.AddJsonFile("hostsettings.json", optional: true);
+        //    builder.Configuration.AddEnvironmentVariables(prefix: "PREFIX_");
+        //    builder.Configuration.AddCommandLine(args);
+
+        //    using IHost host = builder.Build();
+
+        //    // Application code should start here.
+
+        //    await host.RunAsync();
+        //}
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var builder = Host.CreateDefaultBuilder(args);
+             builder.ConfigureAppConfiguration(a =>
+            {
+                a.AddJsonFile("hostsettings.json", optional: true);
+                a.AddEnvironmentVariables(prefix: "PREFIX_");
+                a.AddCommandLine(args);
+                a.SetBasePath(Directory.GetCurrentDirectory());
+            });
+             //builder.Environment.ContentRootPath = Directory.GetCurrentDirectory();
+            //builder.Configuration.AddJsonFile("hostsettings.json", optional: true);
+            //builder.Configuration.AddEnvironmentVariables(prefix: "PREFIX_");
+            //builder.Configuration.AddCommandLine(args);
+
+            using IHost host = builder.Build();
+
+            // Application code should start here.
+
+            await host.RunAsync();
         }
     }
 }
