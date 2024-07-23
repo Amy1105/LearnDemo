f5920b89621e7d70c65efdc148dff4f4e514c9d5
zhuyingying <yingying.zhu@cn.aesc-group.com>
2024-07-23 16:39:42 +0800

    1

---
+++ AutofacDemo/Models/MyComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofacDemo.Models
+{
+    public class MyComponent
+    {
+        public MyComponent() { /* ... */ }
+        public MyComponent(ILogger logger) { /* ... */ }
+        public MyComponent(ILogger logger, IConfigReader reader) { /* ... */ }
+    }
+}
---
+++ AutofacDemo/demo1.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autofacDemo
+{
+    /// <summary>
+    /// 注册
+    /// 1、反射组件
+    /// 2、实例组件
+    /// 3、Lambda表达式组件
+    /// 4、打开通用组件
+    /// 5、服务与组件
+    /// 6、默认注册
+    /// 7、有条件登记
+    /// 8、注册配置
+    /// 9、动态提供的注册
+    /// </summary>
+    internal class demo1
+    {
+        
+    }
+
+    /// <summary>
+    /// 传递参数到注册
+    /// 1、可用参数类型
+    /// 2、带反射分量的参数
+    /// 3、Lambda表达式组件的参数
+    /// </summary>
+    internal class demo2 { }
+
+    /// <summary>
+    /// 1、属性注入
+    /// 2、方法注入
+    /// </summary>
+    internal class demo3 { }
+
+    /// <summary>
+    /// 装配扫描
+    /// 1、扫描类型
+    /// 2、扫描模块
+    /// 3、IIS托管Web应用程序
+    /// </summary>
+    internal class demo4 { }
+
+}
