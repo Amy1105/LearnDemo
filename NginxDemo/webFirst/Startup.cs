b53a86dee5039b4496ee270ae0299b9efc2919b4
zhuyingying <yingying.zhu@cn.aesc-group.com>
2024-08-15 17:31:18 +0800

    update

--- NginxDemo/webFirst/Startup.cs
+++ NginxDemo/webFirst/Startup.cs
@@ -23,6 +23,7 @@ namespace webFirst
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
