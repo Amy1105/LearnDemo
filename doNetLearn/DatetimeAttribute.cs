792cd79755e65f3b4715e8b481d6cd55f6f1c167
zhuyingying <yingying.zhu@cn.aesc-group.com>
2024-09-27 13:31:19 +0800

    add datetime kind

---
+++ doNetLearn/DatetimeAttribute.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doNetLearn
+{
+    /// <summary>
+    /// 此代码示例演示了DateTime Kind、Now和UtcNow属性以及SpecificKind（）、ToLocalTime（）,以及ToUniversalTime（）方法
+    /// </summary>
+    public class DatetimeAttribute
+    {
+        public static void show()
+        {
+            //获取当前时刻的日期和时间，调整为本地时区。
+            DateTime saveNow = DateTime.Now;
+            
+            //获取以协调世界时（UTC）表示的当前时刻的日期和时间。
+            DateTime saveUtcNow = DateTime.UtcNow;
+            DateTime myDt;
+
+            // 显示以UTC和当地时间表示的当前时刻的值和Kind属性。
+
+            DisplayNow("UtcNow: ..........", saveUtcNow);
+            DisplayNow("Now: .............", saveNow);
+            Console.WriteLine();
+
+            // 将当前时刻的Kind属性更改为DateTimeKind。Utc并显示结果。
+
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Utc);
+            Display("Utc: .............", myDt);
+
+            //将当前时刻的Kind属性更改为DateTimeKind。本地并显示结果。
+
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Local);
+            Display("Local: ...........", myDt);
+
+            // 将当前时刻的Kind属性更改为DateTimeKind。未指定并显示结果。
+
+            myDt = DateTime.SpecifyKind(saveNow, DateTimeKind.Unspecified);
+            Display("Unspecified: .....", myDt);
+        }
+
+
+        /// <summary>
+        /// 显示DateTime结构的值和Kind属性、转换为本地时间的DateTime结构和转换为世界时的DateTime结构。
+        /// </summary>
+        public static string datePatt = @"M/d/yyyy hh:mm:ss tt";
+        public static void Display(string title, DateTime inputDt)
+        {
+            DateTime dispDt = inputDt;
+            string dtString;
+
+            // Display the original DateTime.
+
+            dtString = dispDt.ToString(datePatt);
+            Console.WriteLine("{0} {1}, Kind = {2}",
+                              title, dtString, dispDt.Kind);
+
+            // Convert inputDt to local time and display the result.
+            // If inputDt.Kind is DateTimeKind.Utc, the conversion is performed.
+            // If inputDt.Kind is DateTimeKind.Local, the conversion is not performed.
+            // If inputDt.Kind is DateTimeKind.Unspecified, the conversion is
+            // performed as if inputDt was universal time.
+
+            dispDt = inputDt.ToLocalTime();
+            dtString = dispDt.ToString(datePatt);
+            Console.WriteLine("  ToLocalTime:     {0}, Kind = {1}",
+                              dtString, dispDt.Kind);
+
+            // Convert inputDt to universal time and display the result.
+            // If inputDt.Kind is DateTimeKind.Utc, the conversion is not performed.
+            // If inputDt.Kind is DateTimeKind.Local, the conversion is performed.
+            // If inputDt.Kind is DateTimeKind.Unspecified, the conversion is
+            // performed as if inputDt was local time.
+
+            dispDt = inputDt.ToUniversalTime();
+            dtString = dispDt.ToString(datePatt);
+            Console.WriteLine("  ToUniversalTime: {0}, Kind = {1}",
+                              dtString, dispDt.Kind);
+            Console.WriteLine();
+        }
+
+     
+        /// <summary>
+        /// 展示值和kind属性
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="inputDt"></param>
+        public static void DisplayNow(string title, DateTime inputDt)
+        {
+            string dtString = inputDt.ToString(datePatt);
+            Console.WriteLine("{0} {1}, Kind = {2}",
+                              title, dtString, inputDt.Kind);
+        }
+    }
+}
--- doNetLearn/Program.cs
+++ doNetLearn/Program.cs
@@ -1,23 +1,33 @@
 ﻿
-using System;
+using doNetLearn;
 
 
-        // Convert a Continent to a Double.
-        Continent cont = Continent.NorthAmerica;
-        Console.WriteLine("{0:N2}",Convert.ChangeType(cont, typeof(Double)));
+Console.WriteLine("返回指定类型的对象，其值等于指定对象");
+{
 
-        // Convert a Double to a Continent.
-        Double number = 6.0;
-        try
-        {
-            Console.WriteLine("{0}",Convert.ChangeType(number, typeof(Continent)));
-        }
-        catch (InvalidCastException)
-        {
-            Console.WriteLine("Cannot convert a Double to a Continent");
-        }
-        Console.WriteLine("{0}", (Continent)number);
+    Continent cont = Continent.NorthAmerica;
+    Console.WriteLine("{0:N2}", Convert.ChangeType(cont, typeof(Double)));
 
-var d=Convert.ChangeType("2024-09-02", typeof(DateTime));
+    Double number = 6.0;
+    try
+    {
+        Console.WriteLine("{0}", Convert.ChangeType(number, typeof(Continent)));
+    }
+    catch (InvalidCastException)
+    {
+        Console.WriteLine("Cannot convert a Double to a Continent");
+    }
+    Console.WriteLine("{0}", (Continent)number);
 
-Console.WriteLine(d);
+    var d = Convert.ChangeType("2024-09-02", typeof(DateTime));
+    Console.WriteLine(d);
+}
+
+
+
+Console.WriteLine("DateTime kind 属性");
+{
+    DatetimeAttribute.show();
+}
+
+
