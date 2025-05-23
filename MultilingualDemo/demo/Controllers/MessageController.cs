691971f486070c32d4a30e21fa61a7b18d8ad364
zhuyingying <yingying.zhu@cn.aesc-group.com>
2025-05-23 17:08:44 +0800

    update

--- MultilingualDemo/demo/Controllers/MessageController.cs
+++ MultilingualDemo/demo/Controllers/MessageController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using OrderService;
 
 namespace demo.Controllers
@@ -7,7 +8,7 @@ namespace demo.Controllers
     {
         private readonly MessageService messageService;
 
-        public MessageController(MessageService  _messageService)
+        public MessageController(MessageService _messageService)
         {
             messageService = _messageService;
         }
@@ -15,5 +16,31 @@ namespace demo.Controllers
         {
             return View();
         }
+
+
+        public record MethodParam(int id, string name);
+
+        public IActionResult Method1([FromBody] MethodParam methodParam)
+        {
+            return Ok();
+        }
+
+
+        public IActionResult Method2([FromQuery] string name, int id)
+        {
+            return Ok();
+        }
+
+        public IActionResult Method3([FromRoute] int id)
+        {
+            return Ok();
+        }
+
+        public IActionResult Method4([FromForm] MethodParam methodParam)
+        {
+            return Ok();
+
+        }
+
     }
 }
